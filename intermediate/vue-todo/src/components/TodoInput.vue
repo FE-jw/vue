4f121558651186ae735694ff1ca3d1ec89a45def
zai <zaixu91@gmail.com>
2023-07-06 20:18:00 +0900

    vue 중급 스터디 7일차

--- intermediate/vue-todo/src/components/TodoInput.vue
+++ intermediate/vue-todo/src/components/TodoInput.vue
@@ -20,15 +20,15 @@ export default {
 		}
 	},
 	methods: {
-		addTodo: function(){
+		addTodo(){
 			if(this.newTodoItem !== ''){
-				this.$emit('addTodoItem', this.newTodoItem);
+				this.$store.commit('addOneItem', this.newTodoItem);
 				this.clearInput();
 			}else{
 				this.showModal = !this.showModal;
 			}
 		},
-		clearInput: function(){
+		clearInput(){
 			this.newTodoItem = '';
 		}
 	},
