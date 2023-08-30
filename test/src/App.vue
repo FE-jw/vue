b9633b24c9ad891e3d15e35b540fd9c7ac3ea452
zai <zaixu91@gmail.com>
2023-08-30 16:00:58 +0900

    test

--- test/src/App.vue
+++ test/src/App.vue
@@ -1,41 +1,41 @@
 <template>
-	<div class="wrap" :data-theme="theme">
-		<AppHeader @onChangeTheme="changeTheme"></AppHeader>
-		<AppContainer :lang="lang"></AppContainer>
-		<AppFooter></AppFooter>
+	<div class="wrap" :data-theme="store.theme">
+		<app-header></app-header>
+		<app-container></app-container>
+		<app-footer></app-footer>
+		<app-popup v-if="popup.show">
+			<template #head>
+				<div class="pop-tit">팝업 제목</div>
+			</template>
+			<template #body>
+				<div>ESC 눌러도 닫힘..!</div>
+			</template>
+		</app-popup>
 	</div>
 </template>
 
 <script setup>
-import AppHeader from './components/AppHeader.vue';
-import AppContainer from './components/AppContainer.vue';
-import AppFooter from './components/AppFooter.vue';
-import { ref } from 'vue';
+import AppHeader from '@/components/AppHeader.vue';
+import AppContainer from '@/components/AppContainer.vue';
+import AppFooter from '@/components/AppFooter.vue';
+import AppPopup from '@/components/AppPopup.vue';
+import { useStore } from '@/stores/store.js';
+import { usePopup } from '@/stores/popup.js';
 
-const lang = document.documentElement.lang;
-let theme;
-
-if(window.matchMedia('(prefers-color-scheme: dark)').matches){
-	theme = ref('dark');
-}else{
-	theme = ref('light');
-}
-
-const changeTheme = () => {
-	if(theme.value === 'dark'){
-		theme.value = 'light';
-	}else{
-		theme.value = 'dark';
-	}
-};
+const store = useStore();
+const popup = usePopup();
 </script>
 
 <style lang="scss">
 *	{margin:0;padding:0;}
 html	{-webkit-text-size-adjust:none;font-size:10px;}
 button	{border:0;font-family:inherit;font-size:1.4rem;font-weight:inherit;background:none;cursor:pointer;}
-.wrap	{display:flex;flex-direction:column;min-height:100vh;line-height:1.3;font-family:'Pretendard', Arial, Helvetica, sans-serif;font-size:1.4rem;font-weight:200;
+.wrap	{display:flex;flex-direction:column;min-height:100vh;min-height:100dvh;line-height:1.3;font-family:'Pretendard', Arial, Helvetica, sans-serif;font-size:1.4rem;font-weight:200;transition:color 0.2s, background-color 0.2s;
 	&[data-theme='dark']	{color:#fff;background-color:#111;}
 	&[data-theme='light']	{color:#111;background-color:#fff;}
 }
+</style>
+
+<style lang="scss" scoped>
+.pop-tit	{font-weight:900;}
 </style>
---
+++ test/src/components/AppLang.vue
@@ -0,0 +1,43 @@
+<template>
+	<div>
+		<p>만남 인사: {{ multiLang.hello[store.lang] }}</p>
+		<p>헤어짐 인사: {{ multiLang.bye[store.lang] }}</p>
+		<p v-if="multiLang.txt_1[store.lang]">중간체인 경우 제거됨: {{ multiLang.txt_1[store.lang] }}</p>
+		<p v-if="multiLang.txt_2[store.lang]">영문인 경우 제거됨: {{ multiLang.txt_2[store.lang] }}</p>
+		<common-btn @click="changeLang">언어 변경</common-btn>
+		<ul class="reward_1">
+			<li v-for="item in multiLang.reward_1" :key="item.id">
+				<span class="reward">{{ item[store.lang] }}</span>
+				<button type="button">보상 받기</button>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script setup>
+import { useStore } from '@/stores/store.js';
+import { multiLang } from '@/json/multiLang.js';
+
+const store = useStore();
+
+const changeLang = () => {
+	const langState = store.lang;
+
+	switch(langState){
+		case 'ko': store.setLang('en');
+			break;
+		case 'en': store.setLang('zh-hans');
+			break;
+		case 'zh-hans': store.setLang('ko');
+			break;
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+.reward_1	{display:flex;flex-wrap:wrap;gap:2.0rem;list-style:none;
+	li	{flex-shrink:0;width:10.0rem;height:20.0rem;position:relative;background-color:$vue-light;}
+	.reward	{display:flex;justify-content:center;align-items:center;height:4.0rem;font-size:1.6rem;text-align:center;background-color:$vue-dark;}
+	button	{width:100%;height:4.0rem;position:absolute;left:0;bottom:0;font-size:1.6rem;color:inherit;background-color:$vue-dark;}
+}
+</style>
